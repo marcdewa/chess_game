e98e7f20f4f2a0d2d8807f0919c0a14b46afd351
Marcellino <marc_dewa@yahoo.com>
2020-04-01 03:45:18 +0700

    Knight&Bishop Move&Offset

--- src/chessEntities/Knight.java
+++ src/chessEntities/Knight.java
@@ -2,6 +2,7 @@ package chessEntities;
 
 import chess_games.PiecesLocation;
 import chess_games.Position;
+
 public class Knight extends Pieces {
 	
 	public Knight(char player) {
@@ -10,6 +11,15 @@ public class Knight extends Pieces {
 	
 	@Override
 	public boolean canMove(PiecesLocation locFrom,PiecesLocation locTo,Position[][] board) {
+		
+		if(Math.abs(locTo.getFile() - locFrom.getFile()) == 2 && Math.abs(locTo.getRank() - locFrom.getRank()) == 1){
+			return true;
+		}
+		
+		if(Math.abs(locTo.getFile() - locFrom.getFile()) == 1 && Math.abs(locTo.getRank() - locFrom.getRank()) == 2){
+			return true;
+		}
+		
 		return false;
 	}
 }
--- src/chessEntities/Rook.java
+++ src/chessEntities/Rook.java
@@ -10,6 +10,40 @@ public class Rook extends Pieces {
 	
 	@Override
 	public boolean canMove(PiecesLocation locFrom,PiecesLocation locTo,Position[][] board) {
-		return false;
+		
+		if(locFrom.getFile() != locTo.getFile() && locFrom.getRank() != locTo.getRank()){
+			
+			return false;
+		}
+		
+		int offset;
+		
+		if(locFrom.getFile() != locTo.getFile()){
+			
+			offset = (locFrom.getFile() < locTo.getFile())? 1: -1;
+			
+			for(int x = locFrom.getFile() + offset; x != locTo.getFile(); x += offset){
+				//System.out.println(x+""+""+locFrom.getRank());
+				if(board[x][locFrom.getRank()] != null){
+					return false;
+				}
+			}
+		}
+		
+		
+		
+		if(locFrom.getRank() != locTo.getRank()){
+			offset = (locFrom.getRank() < locTo.getRank()) ? 1 : -1;
+			for(int x = locFrom.getRank() + offset; x != locTo.getRank(); x += offset){
+				//System.out.println(x+""+""+locFrom.getFile());
+				if(board[locFrom.getFile()][x] != null){
+					return false;
+				}
+			}
+		}
+		
+		return true;
+		
 	}
+
 }
